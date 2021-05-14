afc6ee876895a6363bcb943e0c676516c4a70998
Eimantas <eim.klimas@gmail.com>
2021-05-14 19:38:20 +0300

    Init

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Eimantas Klimas on 2021-05-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
